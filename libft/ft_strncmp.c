0723cdb67c4393a3ef6eb54421e02f821588b906
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-07-20 09:22:36 +0200

    Ajout de ft_strnstr

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,31 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agonelle <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/19 10:54:21 by agonelle          #+#    #+#             */
-/*   Updated: 2022/07/19 11:13:47 by agonelle         ###   ########.fr       */
+/*   Created: 2022/07/19 11:15:18 by agonelle          #+#    #+#             */
+/*   Updated: 2022/07/19 12:10:50 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char *strrchr(const char *s, int c)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
-	char *cp;
-	char cc;
+	char	*cp1;
+	char	*cp2;
 
 	i = 0;
-	cp = (char *)s;
-	cc = (char) c;
-	while (cp[i] != 0)
+	cp1 = (char*)s1;
+	cp2 = (char*)s2;
+	while (cp1[i] && cp2[i] && cp1[i] != cp2[i] && i < n)
 		i++;
-	while (cp[i] != cc && i != 0)
-		i--;
-	if (cp[i] == cc)
-		return (&cp[i]);
-	return (NULL);
+	return (cp1[i] - cp2[i]);
 }
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,31 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agonelle <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/19 10:54:21 by agonelle          #+#    #+#             */
-/*   Updated: 2022/07/19 11:13:47 by agonelle         ###   ########.fr       */
+/*   Created: 2022/07/19 12:32:55 by agonelle          #+#    #+#             */
+/*   Updated: 2022/07/19 13:13:04 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *strrchr(const char *s, int c)
+char	*strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
-	char *cp;
-	char cc;
+	size_t	j;
+	char cpb;
+	char cpl;
 
+	cpb = (char *)big;
+	cpl = (char *)little;
 	i = 0;
-	cp = (char *)s;
-	cc = (char) c;
-	while (cp[i] != 0)
-		i++;
-	while (cp[i] != cc && i != 0)
-		i--;
-	if (cp[i] == cc)
-		return (&cp[i]);
-	return (NULL);
+	while (cpb[i] != '\0' && i < len)
+	{
+		j = 0;
+		if (cpb[i + j] == cpl[j])
+		
+	}
 }
