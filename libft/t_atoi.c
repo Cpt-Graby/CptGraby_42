69dfc69d1635752051a8bde9cebcf50d7c4605f5
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-07-26 17:12:22 +0200

    Tentative de mettre un fichier de test a jour

---
+++ libft/t_atoi.c
@@ -0,0 +1,53 @@
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int	t_atoi(char *src);
+
+int	main(void)
+{
+	char	src1[] = "";
+	char	src2[] = "100";
+	char	src3[] = "0";
+	char	src4[] = "-100";
+	char	src5[] = "-2147483648";
+
+	printf("Test1");
+	if (t_atoi(src1))
+		printf("OK\n");
+	else
+		printf("KO\n");
+	printf("Test2");
+	if (t_atoi(src2))
+		printf("OK\n");
+	else
+		printf("KO\n");
+	printf("Test3");
+	if (t_atoi(src3))
+		printf("OK\n");
+	else
+		printf("KO\n");
+	printf("Test4");
+	if (t_atoi(src4))
+		printf("OK\n");
+	else
+		printf("KO\n");
+	printf("Test5");
+	if (t_atoi(src5))
+		printf("OK\n");
+	else
+		printf("KO\n");
+ 	return (0);
+}
+
+int	t_atoi(char *src)
+{
+	int	a;
+	int	b;
+	a = ft_atoi(src);
+	b = atoi(src);
+	if (a == b)
+		return (0);
+	else
+		return (1);
+}
