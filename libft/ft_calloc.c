89c9d8e50792d22df82ae88fd6891b4d5dfa1027
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-08-02 01:04:14 +0200

    ajout de enomem dans calloc et creation du fichier de strdup.

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -4,16 +4,19 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*res;
-	int		int_max;
+	int	int_max;
 
 	int_max = 2147483647;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	if (int_max / size < nmemb)
-		return (1);
+	if ((int_max / size) < nmemb)
+	{
+		errno = ENOMEM;
+		return (NULL);
+	}
 	res = malloc(size * nmemb);
-	ft_memset(res, 0, size * nmemb);
+	ft_bzero(res, size * nmemb);
 	return (res);
 
 	//TODO: Checker comment se commenter le message d'erreur avec un overflow.
--- libft/libft.h
+++ libft/libft.h
@@ -14,9 +14,11 @@
 # define LIBFT_H
 
 #include <unistd.h>
+#include <errno.h>
 
-void	ft_bzero(void *s, size_t n);
 int	ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
