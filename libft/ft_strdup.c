daf1e84be0b3630796e78505fe01f8dbbdc3f78c
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-08-02 18:30:24 +0200

    Ajout de la fonction ft_strdup.c

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -1,2 +1,18 @@
 #include "libft.h"
 
+char *strdup(const char *s)
+{
+	size_t	len;
+	size_t	i;
+	char	*cp;
+
+	len = ft_strlen(s);
+	i = 0;
+	cp = malloc(sizeof(*cp) * (len + 1));
+	while ( (s + i) != 0)
+	{
+		(cp + i) = (s + i);
+		i++;
+	}
+	return (cp);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -35,6 +35,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
