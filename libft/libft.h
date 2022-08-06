6ce2753294ed4dad257ac9b3864501ca3f05a498
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-08-06 21:54:52 +0200

    Creation of ft_putchar_fd

--- libft/libft.h
+++ libft/libft.h
@@ -36,6 +36,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
+void	ft_putchar_fd(char c, int fd);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
