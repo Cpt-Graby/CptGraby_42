21a75cd8b77ff7e8a1ad03b101cb852596ae6496
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-10-06 14:20:20 +0200

    Petite maj

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char		*n_str;
+
+	i = 0;
+	n_str = calloc(sizeof(*n_str), ft_strlen(s) + 1);
+	if (!n_str)
+		return (NULL);
+	while (s[i])
+	{
+		n_str[i] = f(i, s[i]);
+		i++;
+	}
+	n_str[i] = '\0';
+	return (n_str);
+}
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,17 +1,17 @@
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+	char *ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*dest;
 	size_t	i;
 	size_t	len2;
 
 	i = 0;
-	len2 = ft_strlen(((char *)s + start));
+	len2 = ft_strlen((s + start));
 	if (len < len2)
 		len2 = len;
 	dest = calloc(sizeof(*dest), len2 + 1);
-	if (dest == NULL)
+	if (dest == NULL || start > ft_strlen(s))
 		return (NULL);
 	while (*(s + start +  i) && i < (len))
 	{
