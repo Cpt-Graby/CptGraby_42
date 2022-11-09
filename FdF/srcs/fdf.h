f5c261a710bf4213817e027c09ad1da9d2856738
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-09 14:43:33 +0100

    Des elements de comprehension de minilibx

---
+++ FdF/srcs/fdf.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agonelle <agonelle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 14:18:50 by agonelle          #+#    #+#             */
+/*   Updated: 2022/11/09 14:35:22 by agonelle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# define WIN_W 600
+# define WIN_H 300
+
+typedef struct	s_vars_w{
+	void	*mlx;
+	void	*win;
+}	t_vars;
+
+typedef struct	s_img_dt {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_lth;
+	int		endian;
+}				t_img_dt;
+
+typedef struct	s_vec2 {
+	int		x;
+	int		y;
+} t_vec2;
+
+#endif
--- FdF/srcs/main.c
+++ FdF/srcs/main.c
@@ -6,44 +6,54 @@
 /*   By: agonelle <agonelle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:55:41 by agonelle          #+#    #+#             */
-/*   Updated: 2022/11/08 17:52:23 by mura             ###   ########.fr       */
+/*   Updated: 2022/11/09 14:42:49 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../42lib/libft.h"
 #include "../mlx.h"
-#include "../fdf.h"
+#include "fdf.h"
 
-typedef struct s_data {
-	void *img;
-	char *addr;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
-}	t_data;
 
-void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int	close_win(int keycode, t_vars *vars)
+{
+	(void) keycode;
+	mlx_destroy_window(vars->mlx, vars->win);
+	exit (0);
+	return (0);
+}
+
+void pixel_2img(t_img_dt *data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color; 
+	dst = data->addr + (y * data->line_lth + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
+void draw
 int fdf_main()
 {
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
+	t_vars		vars;
+	t_img_dt	img;
+	t_vec2		p1;
+	t_vec2		p2;
+	
+	p1.x = 10;
+	p1.y = 10;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
-		       &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	p2.x = 100;
+	p2.y = 100;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, WIN_W, WIN_H, "My first window!");
+	img.img = mlx_new_image(vars.mlx, WIN_W, WIN_H);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lth,
+								&img.endian);
+	d
+	mlx_hook(vars.win, 2, 1L<<0, close_win, &vars);
+	mlx_loop(vars.mlx);
+
 	return (0);
 }
 
