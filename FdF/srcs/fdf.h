f336c61756b7f0ff04a0447453f1141a32f00c2e
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-10 09:21:10 +0100

    Mise a la norme42 v3

--- FdF/srcs/fdf.h
+++ FdF/srcs/fdf.h
@@ -16,22 +16,22 @@
 # define WIN_W 600
 # define WIN_H 300
 
-typedef struct	s_vars_w{
+typedef struct s_vars_w{
 	void	*mlx;
 	void	*win;
 }	t_vars;
 
-typedef struct	s_img_dt {
+typedef struct s_img_dt {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_lth;
 	int		endian;
-}				t_img_dt;
+}	t_img_dt;
 
-typedef struct	s_vec2 {
+typedef struct s_vec2 {
 	int		x;
 	int		y;
-} t_vec2;
+}	t_vec2;
 
 #endif
--- FdF/srcs/main.c
+++ FdF/srcs/main.c
@@ -14,7 +14,6 @@
 #include "../mlx.h"
 #include "fdf.h"
 
-
 int	close_win(int keycode, t_vars *vars)
 {
 	ft_printf("%d", keycode);
@@ -23,7 +22,7 @@ int	close_win(int keycode, t_vars *vars)
 	return (0);
 }
 
-void pixel_2img(t_img_dt *data, int x, int y, int color)
+void	pixel_2img(t_img_dt *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -33,35 +32,33 @@ void pixel_2img(t_img_dt *data, int x, int y, int color)
 
 int	val_in_r(int v, int min, int max)
 {
-	if (v >= min && v <= max) 
+	if (v >= min && v <= max)
 		return (1);
 	else
 		return (0);
 }
 
-int vec2_in_scr(t_vec2 p, int max_h, int max_l)
+int	vec2_in_scr(t_vec2 p, int max_h, int max_l)
 {
 	if ((val_in_r(p.x, 0, max_l) && val_in_r(p.y, 0, max_h))
-			&& (val_in_r(p.x, 0, max_l) && val_in_r(p.y, 0, max_h)))
+		&& (val_in_r(p.x, 0, max_l) && val_in_r(p.y, 0, max_h)))
 		return (1);
 	else
-		return(0);
+		return (0);
 }
 
-
-void draw_line(t_vec2 p1, t_vec2 p2, t_img_dt *data)
+void	draw_line(t_vec2 p1, t_vec2 p2, t_img_dt *data)
 {
-	int	dx;
-	int	dy;
-	float m;
+	int		dx;
+	int		dy;
+	float	m;
 	t_vec2	pi;
-	
-	if (!vec2_in_scr(p1, WIN_H, WIN_W ) && !vec2_in_scr(p2, WIN_H, WIN_W))
+
+	if (!vec2_in_scr(p1, WIN_H, WIN_W) && !vec2_in_scr(p2, WIN_H, WIN_W))
 		ft_putstr_fd("Erreur coordonnee", 1);
 	dx = p2.x - p1.x;
 	dy = p2.y - p1.y;
-	m = dy /dx;
-
+	m = dy / dx;
 	pi.x = p1.x;
 	while (pi.x <= p2.x)
 	{
@@ -71,40 +68,36 @@ void draw_line(t_vec2 p1, t_vec2 p2, t_img_dt *data)
 	}
 }
 
-int fdf_main()
+int	fdf_main( void)
 {
 	t_vars		vars;
 	t_img_dt	img;
 	t_vec2		p1;
 	t_vec2		p2;
-	
+
 	p1.x = 10;
 	p1.y = 10;
-
 	p2.x = 100;
 	p2.y = 200;
-
 	vars.mlx = mlx_init();
 	vars.win = mlx_new_window(vars.mlx, WIN_W, WIN_H, "My first window!");
 	img.img = mlx_new_image(vars.mlx, WIN_W, WIN_H);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lth,
-								&img.endian);
-
+			&img.endian);
 	draw_line(p1, p2, &img);
 	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
 	mlx_hook(vars.win, 17, 0, close_win, &vars);
 	mlx_hook(vars.win, 2, 0, close_win, &vars);
 	mlx_loop(vars.mlx);
-
 	return (0);
 }
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	(void)	argc;
-	(void)	argv;
 	int	err;
-	
+
+	(void) argc;
+	(void) argv;
 	err = fdf_main();
 	return (err);
 }
