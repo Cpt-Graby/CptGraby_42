1022979d8a9ccc68febd550458cf786f3e53f9a2
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-14 22:01:55 +0100

    Fixation des structures et du free

--- FdF/srcs/fdf_file2data.c
+++ FdF/srcs/fdf_file2data.c
@@ -6,7 +6,7 @@
 /*   By: agonelle <agonelle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 14:04:09 by agonelle          #+#    #+#             */
-/*   Updated: 2022/11/14 19:53:28 by kino             ###   ########.fr       */
+/*   Updated: 2022/11/14 21:56:07 by kino             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,12 @@ void print_tabl(t_map *map)
 		i = 0;
 		while ( i < map->column)
 		{
-			ft_printf("%d-", map->tab_line[y].tab_pts[i].x);
+			ft_printf("(%d-", map->tab_line[y].tab_pts[i].x);
 			ft_printf("%d-", map->tab_line[y].tab_pts[i].y);
-			ft_printf("%d\n", map->tab_line[y].tab_pts[i].z);
+			ft_printf("%d) ", map->tab_line[y].tab_pts[i].z);
 			i++;
 		}
+		ft_printf("\n");
 		y++;
 	}
 }
@@ -60,11 +61,9 @@ t_line	*get_next_pts_line(t_map *map, char **tab)
 		while (i < map->line)
 		{
 			new_tab[i] = map->tab_line[i];
-			//for (int y = 0; y < map->column; y++)
-			//	print_vec3(new_tab[i].tab_pts[y]);
 			i++;
 		}
-//	free_t_line(map->tab_line, map->column);
+		free(map->tab_line);
 	}
 	i = 0;
 	while ( i < map->column)
@@ -123,26 +122,6 @@ int	line_2_tab(char *line, t_map *map)
 	return (1);
 }
 
-void spe_prt(int i, int j, t_map *map)
-{
-	int x;
-	int y;
-	(void) map;
-
-	x = 0;
-	y = 0;
-	while (x < i)
-	{
-		y = 0;
-		while (y < j)
-		{
-			//print_vec3(map->tab_line[y]->tab_pts[x]);
-			y++;
-		}
-		x++;
-	}
-}
-
 int	map_parser(int fd, t_map *map)
 {
 	char	*line;
@@ -159,13 +138,12 @@ int	map_parser(int fd, t_map *map)
 	line = get_next_line(fd);
 	while (line)
 	{
-		spe_prt(map->column, map->line, map);
 		if (!line_2_tab(line, map))
 			return (0);
 		line = get_next_line(fd);
 		map->line++;
 	}
-	print_tabl(map);
+	//print_tabl(map);
 	return (1);
 }
 
@@ -187,23 +165,6 @@ int	main_parser(char *path, t_map *map)
 		perror("fdf_file2data.c - map_parser (fd):");
 		return (0);
 	}
-	/*
-	test[0].x = -50;
-	test[0].y = -50;
-	test[0].z = -50;
-	
-	test[1].x = -50;
-	test[1].y = -50;
-	test[1].z = -50;
-	test[2].x = -60;
-	test[2].y = -60;
-	test[2].z = -60;
-
-	print_vec3(test[0]);
-	print_vec3(test[1]);
-	print_vec3(test[2]);
-	free(test);
-	*/
 	close (fd);
 	return (1);
 }
