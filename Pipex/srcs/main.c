62dcfb8ac1f65608ace980da823bfd8814113c1b
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-25 23:44:39 +0100

    Ajout d'une fonction de test basique et d'une regles dans le make file

--- Pipex/srcs/main.c
+++ Pipex/srcs/main.c
@@ -6,20 +6,24 @@
 /*   By: agonelle <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 14:02:16 by agonelle          #+#    #+#             */
-/*   Updated: 2022/11/25 23:13:15 by agonelle         ###   ########.fr       */
+/*   Updated: 2022/11/25 23:40:14 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int test (int fd_inp, int fd_out, char *str)
+{
+	(void) fd_inp;
+	write(fd_out, str, ft_strlen(str));
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
 	int	fd_in;
 	int	fd_out;
-	char	*str;
 
-	str = "J aimerai ajouter cette deuxieme phrase";
 	if (argc < 5)
 	{
 		errno = EINVAL;
@@ -29,8 +33,7 @@ int	main(int argc, char **argv)
 	else
 	{
 		ft_init_fd(argc, argv, &fd_in, &fd_out);
-		write(fd_out, str, ft_strlen(str));
-		write(1, str, ft_strlen(str));
+		test(fd_in, fd_out, argv[argc - 2]);
 	}
 	close(fd_in);
 	close(fd_out);
