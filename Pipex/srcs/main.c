5c24c42c3dba900706e127f7110bfe20ed057add
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2022-11-25 17:09:16 +0100

    file descriptor

--- Pipex/srcs/main.c
+++ Pipex/srcs/main.c
@@ -6,33 +6,70 @@
 /*   By: agonelle <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 14:02:16 by agonelle          #+#    #+#             */
-/*   Updated: 2022/11/24 16:34:35 by agonelle         ###   ########.fr       */
+/*   Updated: 2022/11/25 17:01:09 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../42lib/libft.h"
-#include <errno.h>
-#include <fcntl.h>
+#include "pipex.h"
 
-void	init_fd(char **argu, int *tab, int cas_val)
+int	ft_init_fd_in(char **arg, int cas_val)
 {
+	int	filedesc;
+
 	if (!cas_val)
-		tab[0] = open(argu[2], O_RDONLY);
+		filedesc = open(arg[2], O_RDONLY);
 	else
-		tab[0] = open(argu[1], O_RDONLY);
-	if (tab[0] == -1)
+		filedesc = open(arg[1], O_RDONLY);
+	if (filedesc == -1)
 	{
-		perror("init_fd - file Input");
-		exit(errno);
+		perror("ft_init_fd - file Input");
+		return (-1);
 	}
+	return (filedesc);
+}
+
+int	ft_init_fd_out(char *path, int cas_val)
+{
+	int		filedesc;
+	mode_t	mode;
+
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
 	if (!cas_val)
-		tab[1] = 0;
+		filedesc = open(path, O_APPEND);
+	else
+		filedesc = open(path, O_CREAT | O_WRONLY, mode);
+	if (filedesc == -1)
+	{
+		perror("ft_init_fd - file Output");
+		return (-1);
+	}
+	else
+		return (filedesc);
+}
+
+void	ft_init_fd(int argc, char **argv, int *fd1, int *fd2)
+{
+	int	bonus;
+
+	if (!ft_strncmp(argv[1], "here_doc", 15))
+		bonus = 0;
+	else
+		bonus = 1;
+	*fd1 = ft_init_fd_in(argv, bonus);
+	if (*fd1 == -1)
+		exit(-1);
+	*fd2 = ft_init_fd_out(argv[argc - 1], bonus);
+	if (*fd2 == -1)
+	{
+		close(*fd1);
+		exit(-1);
+	}
 }
 
 int	main(int argc, char **argv)
 {
-	int	fd[2];
-	int	res;
+	int	fd_in;
+	int	fd_out;
 
 	if (argc < 5)
 	{
@@ -42,9 +79,10 @@ int	main(int argc, char **argv)
 	}
 	else
 	{
-		if (!ft_strncmp(argv[1], "here_doc", 15))
-			 init_fd(argv, &fd, 0);
-		else
-			init_fd(argv, &fd, 1);
+		ft_init_fd(argc, argv, &fd_in, &fd_out);
+		write(fd_out, "an error writing to standard out\n", 70);
 	}
+	close(fd_in);
+	close(fd_out);
+	return (0);
 }
