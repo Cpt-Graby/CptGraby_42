40ca37fe5f291cd7f83111457c6f1bc07ed3b338
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2023-01-30 14:22:54 +0100

    Oups

--- Push_swap/srcs/push_swap_sol_5.c
+++ Push_swap/srcs/push_swap_sol_5.c
@@ -6,7 +6,7 @@
 /*   By: agonelle <agonelle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 16:17:10 by agonelle          #+#    #+#             */
-/*   Updated: 2023/01/30 09:25:56 by agonelle         ###   ########.fr       */
+/*   Updated: 2023/01/30 14:21:02 by agonelle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,20 @@ int	sort_5_element(t_pi_f *pile_frame, char **tab, int len)
 		return (-1);
 	pile_ind = set_frame(&lec_head_c, pile_frame->head_b, len);
 	set_index(pile_frame, pile_ind);
-	while (pile_ind->f_elem_a->index != 0 && pile_ind->f_elem_a->index != 1)
-		rotate(pile_ind, 'a', 1);
-	push(pile_ind, 'b', 1);
-	while (pile_ind->f_elem_a->index != 0 && pile_ind->f_elem_a->index != 1)
-		rotate(pile_ind, 'a', 1);
-	push(pile_ind, 'b', 1);
-	if (pile_ind->f_elem_b->index < pile_ind->f_elem_b->next->index)
-		rotate(pile_ind, 'b', 1);
-	sort_3_element(pile_ind);
-	push(pile_ind, 'a', 1);
-	push(pile_ind, 'a', 1);
+	if (!check_order_index(pile_ind->f_elem_a, pile_ind->len))
+	{
+		while (pile_ind->f_elem_a->index != 0 && pile_ind->f_elem_a->index != 1)
+			rotate(pile_ind, 'a', 1);
+		push(pile_ind, 'b', 1);
+		while (pile_ind->f_elem_a->index != 0 && pile_ind->f_elem_a->index != 1)
+			rotate(pile_ind, 'a', 1);
+		push(pile_ind, 'b', 1);
+		if (pile_ind->f_elem_b->index < pile_ind->f_elem_b->next->index)
+			rotate(pile_ind, 'b', 1);
+		sort_3_element(pile_ind);
+		push(pile_ind, 'a', 1);
+		push(pile_ind, 'a', 1);
+	}
 	free_frame(pile_ind);
 	return (0);
 }
@@ -47,20 +50,16 @@ int	sort_4_element(t_pi_f *pile_frame, char **tab, int len)
 		return (-1);
 	pile_ind = set_frame(&lec_head_c, pile_frame->head_b, len);
 	set_index(pile_frame, pile_ind);
-	push(pile_ind, 'b', 1);
-	sort_3_element(pile_ind);
-	if (pile_ind->f_elem_b->index == 3)
+	if (!check_order_index(pile_ind->f_elem_a, pile_ind->len))
 	{
+		while (pile_ind->f_elem_a->index != 0 && pile_ind->f_elem_a->index != 1)
+			rotate(pile_ind, 'a', 1);
+		push(pile_ind, 'b', 1);
+		if (pile_ind->f_elem_b->index < pile_ind->f_elem_b->next->index)
+			rotate(pile_ind, 'b', 1);
+		sort_3_element(pile_ind);
 		push(pile_ind, 'a', 1);
-		rotate(pile_ind, 'a', 1);
-		return (0);
 	}
-	while ((pile_ind->f_elem_b->index + 1) != pile_ind->f_elem_a->index)
-		rotate(pile_ind, 'a', 1);
-	printf_piles(pile_ind);
-	push(pile_ind, 'a', 1);
-	while (pile_ind->f_elem_a->index != 0)
-		rotate(pile_ind, 'a', 1);
 	free_frame(pile_ind);
 	return (0);
 }
